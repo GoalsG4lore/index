6c596fbd7dd80352fe7734287bc88a97bbca3e30
James Cleworth <>
2020-05-04 18:55:43 +0100

    Quick draft to fetch data from google spreadsheet. Am currently using a sheet I published. Will need to solidify this and see how this works with Rob's data

--- js/main.js
+++ js/main.js
@@ -5,3 +5,23 @@ $('#aboutLink').click(function(){
 $('#notificationClose').click(function(){
 	$('.notification').hide();
 })
+
+/* retrieving jackpot num */
+
+function getJackpot() {
+	var tabletop = Tabletop.init({ 
+  		key: '1-t6S5Bnp6bKjHsQfiA82xbBRF1tCSS_wGa-NrHDnAIA', 
+  		callback: showJackpot
+	});
+}
+
+
+function showJackpot(data) {
+	var jackPot = data.Sheet1.columnNames[4];
+	$('#jackpot').html(jackPot);
+	setInterval(function(){ getJackpot(); }, 60000);
+}
+
+(function() {
+  getJackpot();
+})()
