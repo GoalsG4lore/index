a4c8b90e877c42a506a7fb86aac578b3567a5b54
James Cleworth <>
2020-05-05 13:33:17 +0100

    Played around with the footer styling and removed redundant JavaScript

--- js/main.js
+++ js/main.js
@@ -1,14 +1,4 @@
-$('#aboutLink').click(function(){
-	$('.home__content, .about__content').toggle();
-});
-
-$('#notificationClose').click(function(){
-	$('.notification').hide();
-})
-
-/* retrieving jackpot num */
-
-
 /* firing Jackpot */
+
 var getJackpot = new JackPotConfig();
 getJackpot.retrieveJackpot();
