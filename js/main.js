1102ac4ab0b28fe5101e5da6b471aff5b6c37633
James Cleworth <>
2020-05-05 11:40:47 +0100

    Sorted out white space below footer and trying to divide up the JavaScript to have its own config section for the jackpot

--- js/main.js
+++ js/main.js
@@ -8,19 +8,7 @@ $('#notificationClose').click(function(){
 
 /* retrieving jackpot num */
 
-function getJackpot() {
-	var tabletop = Tabletop.init({ 
-  		key: '13tMhyefF5Z9oy6iIFVAY_8a46QY566NHTS1V5CpJxn4', 
-  		callback: showJackpot
-	});
-}
 
-function showJackpot(data) {
-	jackpot = data.Jackpot.elements[0]["Latest Jackpot"];
-	$('#jackpot').html(jackpot);
-	setInterval(function(){ getJackpot(); }, 60000);
-}
-
-(function() {
-  getJackpot();
-})()
+/* firing Jackpot */
+var getJackpot = new JackPotConfig();
+getJackpot.retrieveJackpot();
