776c1f9101ebaef3272f26e418ae2ae774feb48d
James Cleworth <>
2020-05-07 21:22:03 +0100

    Big redesign of the home page and added a new section

--- js/main.js
+++ js/main.js
@@ -2,3 +2,28 @@
 
 var getJackpot = new JackPotConfig();
 getJackpot.retrieveJackpot();
+
+/* countdown timer */
+
+var tenMinutes = 60 * 10;
+
+startTimer(tenMinutes)
+
+function startTimer(duration) {
+    var timer = duration;
+    setInterval(function () {
+	minutes = parseInt(timer / 60, 10);
+	seconds = parseInt(timer % 60, 10);
+	minutes = minutes < 10 ? "0" + minutes : minutes;
+	seconds = seconds < 10 ? "0" + seconds : seconds;
+   	$('#countdownTimer').html(minutes + ":" + seconds);
+
+   	--timer;
+
+   	if (timer <= 0 ) {
+   		return;
+   	}
+
+    }, 1000);
+
+}
