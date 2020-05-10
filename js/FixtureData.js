10faa9ccc337bbe384c55a436109d49a7e2150b0
James Cleworth <>
2020-05-10 22:06:53 +0100

    Added more additions to the ajax

--- js/FixtureData.js
+++ js/FixtureData.js
@@ -1,11 +1,14 @@
-console.log("HIT")
 $.ajax({
   type: 'GET',
   url: "https://api.football-data.org/v2/matches",
   headers: { Authorization: $`Bearer ${localStorage.getItem("c69c41fd021b4517b588207dd31861b2")}` },
-
-  success: function(result){
-    console.log(result);
-  }
+  dataType: "jsonp",
+  success: function(data){
+    console.log(data);
+  },
+   error: function (xhr, ajaxOptions, thrownError) {
+        console.log(xhr.status);
+        console.log(thrownError);
+      },
 
 });
