af0a76870ff64d5f22bb7e5209ccea7bda23352c
James Cleworth <>
2020-05-11 07:13:03 +0100

    Commented out fixture data code for now and implemented the jackpot value back into the site

--- js/FixtureData.js
+++ js/FixtureData.js
@@ -1,14 +1,16 @@
-$.ajax({
-  type: 'GET',
-  url: "https://api.football-data.org/v2/matches",
-  headers: { Authorization: $`Bearer ${localStorage.getItem("c69c41fd021b4517b588207dd31861b2")}` },
-  dataType: "jsonp",
-  success: function(data){
-    console.log(data);
-  },
-   error: function (xhr, ajaxOptions, thrownError) {
-        console.log(xhr.status);
-        console.log(thrownError);
-      },
+// setTimeout(function(){ 
+  
+// $.ajax({
+//   url: "https://api.football-data.org/v2/matches",
+//   data: {key: "c69c41fd021b4517b588207dd31861b2"},
+//   dataType: "jsonp",
+//   success: function(data){
+//     console.log("HIT");
+//   },
+//    error: function (xhr, ajaxOptions, thrownError) {
+//         console.log(xhr.status);
+//         console.log(thrownError);
+//       },
 
-});
+// });
+// }, 3000);
--- js/Jackpot.js
+++ js/Jackpot.js
@@ -4,7 +4,6 @@ var JackPotConfig = function () {
 
 	JackPotConf.retrieveJackpot = function() {
 		var tabletop = Tabletop.init({ 
-				// key: '13tMhyefF5Z9oy6iIFVAY_8a46QY566NHTS1V5CpJxn4',
 				key: '13tMhyefF5Z9oy6iIFVAY_8a46QY566NHTS1V5CpJxn4', 
 				callback: showJackpot,
 				// debug: true,
@@ -12,9 +11,7 @@ var JackPotConfig = function () {
 	}
 
 	function showJackpot (data, object) {
-		console.log(data.JamesTest.elements)
-		jackpot = data[0]["Latest Jackpot"];
+		jackpot = data.Jackpot.elements[0]["Latest Jackpot"];
 		$('#jackpot').html(jackpot);
-		//setInterval(function(){ JackPotConf.retrieveJackpot(); }, 60000);
 	}
 }
