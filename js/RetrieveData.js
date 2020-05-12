41f03c3969cfb5a39645aa1f608ff1eb8bb7d76a
James Cleworth <>
2020-05-12 19:59:36 +0100

    Styled the currentGW table to have a GG styling and removed redundant data from google sheets so we don't retrieve all of the rows

--- js/RetrieveData.js
+++ js/RetrieveData.js
@@ -5,6 +5,8 @@ var RetrieveDataConfig = function () {
 	RetrieveDataConf.countdown = ko.observable();
 	RetrieveDataConf.currentGameweek = ko.observableArray([]);
 
+	var gameWeekArray = [];
+
 	RetrieveDataConf.retrieveData = function() {
 		var tabletop = Tabletop.init({ 
 				key: '13tMhyefF5Z9oy6iIFVAY_8a46QY566NHTS1V5CpJxn4', 
@@ -13,15 +15,23 @@ var RetrieveDataConfig = function () {
 	};
 		
 	RetrieveDataConf.callBackData = function(data) {
-
 		/* for jackpot */
 		var jackPot = data.Jackpot.elements[0]["Latest Jackpot"];
 		RetrieveDataConf.jackPotData(jackPot);
 
 		/* for current gameweek */
-		var currentGameweek = data["Current Gameweek"].elements;
-		currentGameweek.shift() //removing first item in array (which are the headers)
-		RetrieveDataConf.currentGameweek([currentGameweek])
+		var currentGameweekData = data["Current Gameweek"].elements;
+
+		for (var i = 0; i < currentGameweekData.length; i++) {
+			if (currentGameweekData[i].Ref.length !== 0) {
+				gameWeekArray.push(currentGameweekData[i]);
+			} else {
+				break;
+			}
+		}
+
+		gameWeekArray.shift(); //removing first item in array (which are the headers)
+		RetrieveDataConf.currentGameweek([gameWeekArray]);
 
 		/* for countdown */
 		var countdown = data["Deadline"].elements[0]["Time Until Deadline"];
